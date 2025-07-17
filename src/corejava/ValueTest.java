b7cc251fbe3c05c77ad725d8652d1c244c766dad
dailiuyangmac <17600608814@163.com>
2025-07-17 22:05:13 +0800

    【java基本知识】 java是值传递还是引用传递  2025年7月17号 00：30

---
+++ src/corejava/ValueTest.java
@@ -0,0 +1,40 @@
+package corejava;
+
+public class ValueTest {
+				/**
+					* 2025/7/17
+					* URL ：
+					* 题目描述：
+					**/
+				
+				
+				/**
+					* @Param StringBuilder sb 复制的sb的引用
+					*/
+				public static void valueStringBuilder(StringBuilder sb) {
+								// 通过拷贝的引用，对值进行修改
+								sb.append(" world");
+								// 拷贝的引用指向新对象,原本引用值不变
+								sb = new StringBuilder("world");
+								System.out.println(sb);
+				}
+				
+				public static void valueInt(int a) {
+								a++;
+				}
+				
+				public static void main(String[] args) {
+								System.out.println("===============Java是值传递，不是引用传递=====================");
+								System.out.println("===============引用对象：拷贝引用地址=====================");
+								StringBuilder sb = new StringBuilder("hello");
+								valueStringBuilder(sb);
+								System.out.println(sb);
+								
+								System.out.println("--------------------基本类型：值传递-----------------------------");
+								int a = 0;
+								valueInt(a);
+								System.out.println(a);
+				}
+				
+				
+}
